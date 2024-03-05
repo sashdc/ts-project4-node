c3e0669bddf4bf476ad38cb9182ea5462b7746f2
sashdc <saharshdc@gmail.com>
2024-03-05 11:21:34 -0500

    cleaned up code

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,16 +1,19 @@
-import { Router } from 'express';
-import { createTodo, getTodos, updateTodo, deleteTodo  } from '../controllers/todos';
-import { get } from 'https';
-
+import { Router } from "express";
+import {
+  createTodo,
+  getTodos,
+  updateTodo,
+  deleteTodo,
+} from "../controllers/todos";
 
 const router = Router();
 
-router.post('/', createTodo);
+router.post("/", createTodo);
 
-router.get('/', getTodos);
+router.get("/", getTodos);
 
-router.patch('/:id', updateTodo);
+router.patch("/:id", updateTodo);
 
-router.delete('/:id', deleteTodo);
+router.delete("/:id", deleteTodo);
 
 export default router;
